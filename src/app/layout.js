bba1f93b8a343be56cafeedfba905cf293919f3c
Raj Singh <rajsinghchandala@gmail.com>
2026-07-02 22:20:53 +0530

    Add favicon using shivaling image

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,10 @@ import "./globals.css";
 export const metadata = {
   title: "Prachin Baurahwa Mahadev Shiv Mandir | Kushinagar",
   description: "Official website for the ancient Prachin Baurahwa Mahadev Shiv Mandir located in Gram Patharwa, Kushinagar, UP.",
+  icons: {
+    icon: "/images/shivaling_canopy.jpg",
+    apple: "/images/shivaling_canopy.jpg",
+  },
 };
 
 export default function RootLayout({ children }) {
